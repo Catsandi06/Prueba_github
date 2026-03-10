a1d66f05634aa1347a5990ddf0008ca25d8872a1
Nichel Morales <usr3742@salesianos_lacuesta.net>
2026-03-10 13:49:38 +0000

    prueba conflicto1

--- src/App.java
+++ src/App.java
@@ -1,5 +1,11 @@
+
+import net.salesianos.persona.Persona;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Hola clase 1º Dam");
+        Persona p1 = new Persona("pepe", "rosas", 25);
+        System.out.println(p1);
     }
+
 }
--- src/net/salesianos/persona/Persona.java
+++ src/net/salesianos/persona/Persona.java
@@ -1,5 +1,34 @@
 package net.salesianos.persona;
 
 public class Persona {
+    private String nombre;
+    private String apellidos;
+    private int edad;
+
+    public Persona(String nombre, String apellidos, int edad) {
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.edad = edad;
+    }
+
+    public String getnombre() {
+        return nombre;
+    }
+
+    public void setnombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setedad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getapellidos() {
+        return apellidos;
+    }
+
+    public void setapellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
 
 }
